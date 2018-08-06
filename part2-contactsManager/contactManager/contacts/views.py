b0a6d39ed460d9bb33f28ae8d26414e308f6b3ca
TalOmari <talomari592@gmail.com>
2018-08-06 17:28:57 +0300

    python work shop

--- part2-contactsManager/contactManager/contacts/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Contact
-
-# Create your views here.
-#your code is here 
